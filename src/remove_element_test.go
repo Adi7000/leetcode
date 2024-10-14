0f90c477430c035601e7f05817aac42a8d1be5c9
Aditya Goyal <agoyal7000@gmail.com>
2024-10-13 21:07:23 -0600

    TI-150 27.Remove Element added more tests

--- src/remove_element_test.go
+++ src/remove_element_test.go
@@ -14,3 +14,21 @@ func TestRemoveElement(t *testing.T) {
 	assert.Equal(t, expected, actual)
 	assert.Equal(t, expectedNums, nums)
 }
+
+func TestRemoveElementNoElement(t *testing.T) {
+	nums := []int{0, 1, 2, 2, 3, 0, 4, 2}
+	actual := removeElement(nums, 7)
+	expected := 8
+	expectedNums := []int{0, 1, 2, 2, 3, 0, 4, 2}
+	assert.Equal(t, expected, actual)
+	assert.Equal(t, expectedNums, nums)
+}
+
+func TestRemoveElementFirstELement(t *testing.T) {
+	nums := []int{0, 1, 2, 2, 3, 0, 4, 2}
+	actual := removeElement(nums, 0)
+	expected := 6
+	expectedNums := []int{2, 1, 2, 2, 3, 4, 0, 0}
+	assert.Equal(t, expected, actual)
+	assert.Equal(t, expectedNums, nums)
+}
