733337ec6d58246da62fee1608ce28ceb1c35c09
Aditya Goyal <agoyal7000@gmail.com>
2024-10-14 14:19:45 -0600

    PL 198.House Robber optimized
    Solved with dynamic programming in O(n^1.5)
    Used a top-down approach (memoization)

--- src/house_robber.go
+++ src/house_robber.go
@@ -1,21 +1,40 @@
 package leetcode
 
 func rob(nums []int) int {
-	if len(nums) == 0 {
-		return 0
-	} else if len(nums) == 1 {
-		return nums[0]
-	} else if len(nums) == 2 {
-		return max(nums[0], nums[1])
+	//Initialize
+	robbedHouseSliceToAmountMap := map[int]map[int]int{}
+	for i := 0; i <= len(nums); i++ {
+		robbedHouseSliceToAmountMap[i] = map[int]int{}
 	}
-	//Partition into 2 slices in the middle
-	//Note middle always belongs to the right side
-	middle := (len(nums)) / 2
 
-	//We have to prevent adjacent houses from being robbed
-	//In case 1 assume last house of p1 isn't robbed
-	case1 := rob(nums[:middle-1]) + rob(nums[middle:])
-	//In case 2 assume first house of p2 isn't robbed
-	case2 := rob(nums[:middle]) + rob(nums[middle+1:])
-	return max(case1, case2)
+	return robPartition(nums, 0, len(nums), robbedHouseSliceToAmountMap)
+}
+
+func robPartition(nums []int, low int, high int, robbedHouseSliceToAmountMap map[int]map[int]int) int {
+	numHouses := high - low
+	//Check if problem already solved
+	amount, exists := robbedHouseSliceToAmountMap[low][high]
+	if exists {
+		return amount
+		//Base cases
+	} else if numHouses == 0 {
+		amount = 0
+	} else if numHouses == 1 {
+		amount = nums[low]
+	} else if numHouses == 2 {
+		amount = max(nums[low], nums[low+1])
+	} else {
+		//Partition into 2 slices in the middle
+		//Note middle always belongs to the right side
+		middle := low + numHouses/2
+
+		//We have to prevent adjacent houses from being robbed
+		//In case 1 assume last house of p1 isn't robbed
+		case1 := robPartition(nums, low, middle-1, robbedHouseSliceToAmountMap) + robPartition(nums, middle, high, robbedHouseSliceToAmountMap)
+		//In case 2 assume first house of p2 isn't robbed
+		case2 := robPartition(nums, low, middle, robbedHouseSliceToAmountMap) + robPartition(nums, middle+1, high, robbedHouseSliceToAmountMap)
+		amount = max(case1, case2)
+	}
+	robbedHouseSliceToAmountMap[low][high] = amount
+	return amount
 }
