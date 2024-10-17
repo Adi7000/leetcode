9f934c38a22c0476332053a714771db2627cb7de
Aditya Goyal <agoyal7000@gmail.com>
2024-10-16 21:12:30 -0600

    TI-150 238.Product of Array Except Self solved
    Solved in O(n) time with O(n) extra memory

---
+++ src/product_except_self.go
@@ -0,0 +1,31 @@
+package leetcode
+
+func productExceptSelf(nums []int) []int {
+	if len(nums) < 2 {
+		return nil
+	}
+
+	runningProductForward := make([]int, len(nums))
+	runningProductBackward := make([]int, len(nums))
+	productExceptSelfNums := make([]int, len(nums))
+
+	//Calculate running product like Ex. {1, 2, 3, 4} -> {1, 2, 6, 24}
+	runningProductForward[0] = nums[0]
+	for i := 1; i < len(nums); i++ {
+		runningProductForward[i] = runningProductForward[i-1] * nums[i]
+	}
+
+	//Calculate running product like  Ex. {1, 2, 3, 4} -> {24, 24, 12, 4}
+	runningProductBackward[len(nums)-1] = nums[len(nums)-1]
+	for i := len(nums) - 2; i >= 0; i-- {
+		runningProductBackward[i] = runningProductBackward[i+1] * nums[i]
+	}
+
+	//The two neighbouring runningProducts are the prefix and suffix
+	productExceptSelfNums[0] = runningProductBackward[1]
+	productExceptSelfNums[len(nums)-1] = runningProductForward[len(nums)-2]
+	for i := 1; i < len(nums)-1; i++ {
+		productExceptSelfNums[i] = runningProductForward[i-1] * runningProductBackward[i+1]
+	}
+	return productExceptSelfNums
+}
