ab3ec01ca8f82a5f5ef0ca572e1601121d2b6e36
Aditya Goyal <agoyal7000@gmail.com>
2024-10-16 21:38:28 -0600

    TI-150 238.Product of Array Optimized Memory
    Solved in O(n) time and O(1) extra memory

--- src/product_except_self.go
+++ src/product_except_self.go
@@ -9,13 +9,13 @@ func productExceptSelf(nums []int) []int {
 	runningProductBackward := make([]int, len(nums))
 	productExceptSelfNums := make([]int, len(nums))
 
-	//Calculate running product like Ex. {1, 2, 3, 4} -> {1, 2, 6, 24}
+	//Calculate running product forward like Ex. {1, 2, 3, 4} -> {1, 2, 6, 24}
 	runningProductForward[0] = nums[0]
 	for i := 1; i < len(nums); i++ {
 		runningProductForward[i] = runningProductForward[i-1] * nums[i]
 	}
 
-	//Calculate running product like  Ex. {1, 2, 3, 4} -> {24, 24, 12, 4}
+	//Calculate running product backward like  Ex. {1, 2, 3, 4} -> {24, 24, 12, 4}
 	runningProductBackward[len(nums)-1] = nums[len(nums)-1]
 	for i := len(nums) - 2; i >= 0; i-- {
 		runningProductBackward[i] = runningProductBackward[i+1] * nums[i]
@@ -27,5 +27,30 @@ func productExceptSelf(nums []int) []int {
 	for i := 1; i < len(nums)-1; i++ {
 		productExceptSelfNums[i] = runningProductForward[i-1] * runningProductBackward[i+1]
 	}
+	return productExceptSelfNums
+}
+
+func productExceptSelfMemOptimized(nums []int) []int {
+	if len(nums) < 2 {
+		return nil
+	}
+	productExceptSelfNums := make([]int, len(nums))
+	runningProductForward := 1
+
+	//Calculate running product backward like  Ex. {1, 2, 3, 4} -> {24, 24, 12, 4}
+	productExceptSelfNums[len(nums)-1] = nums[len(nums)-1]
+	for i := len(nums) - 2; i >= 0; i-- {
+		productExceptSelfNums[i] = productExceptSelfNums[i+1] * nums[i]
+	}
+
+	//Calculate running product forward like Ex. {1, 2, 3, 4} -> {1, 2, 6, 24}
+	//No need to store RPF so simultaneously set the product except self value
+	for i := 0; i < len(nums)-1; i++ {
+		productExceptSelfNums[i] = runningProductForward * productExceptSelfNums[i+1]
+		runningProductForward *= nums[i]
+	}
+
+	productExceptSelfNums[len(nums)-1] = runningProductForward
+
 	return productExceptSelfNums
 }
--- src/product_except_self_test.go
+++ src/product_except_self_test.go
@@ -8,23 +8,29 @@ import (
 
 func TestProductExceptSelfExample1(t *testing.T) {
 	nums := []int{1, 2, 3, 4}
-	actual := productExceptSelf(nums)
+	actual := productExceptSelfMemOptimized(nums)
 	expected := []int{24, 12, 8, 6}
 	assert.Equal(t, expected, actual)
 }
 func TestProductExceptSelfExample2(t *testing.T) {
 	nums := []int{-1, 1, 0, -3, 3}
-	actual := productExceptSelf(nums)
+	actual := productExceptSelfMemOptimized(nums)
 	expected := []int{0, 0, 9, 0, 0}
+	assert.Equal(t, expected, actual)
+}
+func TestProductExceptSelfNonOneBoundaryValues(t *testing.T) {
+	nums := []int{4, 2, 3, 4}
+	actual := productExceptSelfMemOptimized(nums)
+	expected := []int{24, 48, 32, 24}
 	assert.Equal(t, expected, actual)
 }
 func TestProductExceptSelfNoNums(t *testing.T) {
 	nums := []int{}
-	actual := productExceptSelf(nums)
+	actual := productExceptSelfMemOptimized(nums)
 	assert.Nil(t, actual)
 }
 func TestProductExceptSelfOneNum(t *testing.T) {
 	nums := []int{1}
-	actual := productExceptSelf(nums)
+	actual := productExceptSelfMemOptimized(nums)
 	assert.Nil(t, actual)
 }
