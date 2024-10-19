33defb5f3d66e4b394a2e2f9682b9a6dcb586aa5
Aditya Goyal <agoyal7000@gmail.com>
2024-10-18 21:31:33 -0600

    TI-150 134.Gas Station added tests

---
+++ src/gas_station_test.go
@@ -0,0 +1,32 @@
+package leetcode
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestCanCompleteCircuitExample1(t *testing.T) {
+	gas := []int{1, 2, 3, 4, 5}
+	cost := []int{3, 4, 5, 1, 2}
+	actual := canCompleteCircuit(gas, cost)
+	assert.Equal(t, 3, actual)
+}
+func TestCanCompleteCircuitExample2(t *testing.T) {
+	gas := []int{2, 3, 4}
+	cost := []int{3, 4, 3}
+	actual := canCompleteCircuit(gas, cost)
+	assert.Equal(t, -1, actual)
+}
+func TestCanCompleteCircuitOneReachableLocation(t *testing.T) {
+	gas := []int{2}
+	cost := []int{2}
+	actual := canCompleteCircuit(gas, cost)
+	assert.Equal(t, 0, actual)
+}
+func TestCanCompleteCircuitOneUnreachableLocation(t *testing.T) {
+	gas := []int{1}
+	cost := []int{2}
+	actual := canCompleteCircuit(gas, cost)
+	assert.Equal(t, -1, actual)
+}
