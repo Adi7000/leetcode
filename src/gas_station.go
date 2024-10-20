42ffedd43f156cb826824aa997f71086ab496492
Aditya Goyal <agoyal7000@gmail.com>
2024-10-20 01:23:08 -0600

    TI-150 134.Gas Station optimized
    Solved in O(n)

--- src/gas_station.go
+++ src/gas_station.go
@@ -6,42 +6,25 @@ func canCompleteCircuit(gas []int, cost []int) int {
 	for i := 0; i < len(gas); i++ {
 		gas[i] -= cost[i]
 	}
-	if len(gas) == 1 && gas[0] >= 0 {
-		return 0
-	}
-	//Solved by brute force
-	for start := 0; start < len(gas)-1; start++ {
-		remainingGas := gas[start]
-		if remainingGas < 0 {
-			continue
-		}
-		currentStation := start + 1
-		for {
-			remainingGas += gas[currentStation]
-			if remainingGas < 0 {
-				break
-			}
-			if currentStation != len(gas)-1 {
-				currentStation++
-			} else {
-				currentStation = 0
-			}
-			if currentStation == start {
+	//Try to start at the last station and test if circuit can be completed
+	//if not, try to see if the second last station lets it be completed
+	//and so on
+	start := len(gas) - 1
+	maxReachable := 0
+	remainingGas := gas[start]
+	for {
+		if remainingGas >= 0 {
+			if maxReachable == start {
 				return start
 			}
+			remainingGas += gas[maxReachable]
+			maxReachable++
+		} else {
+			if maxReachable == start {
+				return -1
+			}
+			start--
+			remainingGas += gas[start]
 		}
 	}
-	remainingGas := gas[len(gas)-1]
-	currentStation := 0
-	for {
-		remainingGas += gas[currentStation]
-		if remainingGas < 0 {
-			break
-		}
-		currentStation++
-		if currentStation == len(gas)-1 {
-			return len(gas) - 1
-		}
-	}
-	return -1
 }
