7096c159760b8e2bea2df06789a3e0b67a980755
Aditya Goyal <agoyal7000@gmail.com>
2024-10-24 21:44:25 -0600

    TI-150 Search 2D Matrix added tests

---
+++ src/search_matrix_test.go
@@ -0,0 +1,36 @@
+package leetcode
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestSearchMatrixExample1(t *testing.T) {
+	matrix := [][]int{
+		[]int{1, 3, 5, 7},
+		[]int{10, 11, 16, 20},
+		[]int{23, 30, 34, 60},
+	}
+	actual := searchMatrix(matrix, 3)
+	assert.Equal(t, true, actual)
+}
+func TestSearchMatrixExample2(t *testing.T) {
+	matrix := [][]int{
+		[]int{1, 3, 5, 7},
+		[]int{10, 11, 16, 20},
+		[]int{23, 30, 34, 60},
+	}
+	actual := searchMatrix(matrix, 13)
+	assert.Equal(t, false, actual)
+}
+func TestSearchMatrixEmpty(t *testing.T) {
+	matrix := [][]int{}
+	actual := searchMatrix(matrix, 9)
+	assert.Equal(t, false, actual)
+}
+func TestSearchMatrixOneRow(t *testing.T) {
+	matrix := [][]int{[]int{1, 3, 5, 7}}
+	actual := searchMatrix(matrix, 5)
+	assert.Equal(t, false, actual)
+}
