c6e4deeaffe8d5a67af7a63b4cfe266b5c67e015
Aditya Goyal <agoyal7000@gmail.com>
2024-10-25 21:45:36 -0600

    TI-150 4.Median of Two Sorted Arrays added tests

---
+++ src/median_two_sorted_arrays_test.go
@@ -0,0 +1,22 @@
+package leetcode
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestFindMedianSortedArraysExample1(t *testing.T) {
+	nums1 := []int{1, 3}
+	nums2 := []int{2}
+
+	actual := findMedianSortedArrays(nums1, nums2)
+	assert.Equal(t, 2.0, actual)
+}
+func TestFindMedianSortedArraysExample2(t *testing.T) {
+	nums1 := []int{1, 2}
+	nums2 := []int{3, 4}
+
+	actual := findMedianSortedArrays(nums1, nums2)
+	assert.Equal(t, 2.5, actual)
+}
