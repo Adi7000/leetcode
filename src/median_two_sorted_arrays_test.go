b2d1ef10c001676c8bc6b4450f154402cca26335
Aditya Goyal <agoyal7000@gmail.com>
2024-10-27 23:33:37 -0600

    TI-150 4.Median of Two Sorted Arrays added tests

--- src/median_two_sorted_arrays_test.go
+++ src/median_two_sorted_arrays_test.go
@@ -20,3 +20,10 @@ func TestFindMedianSortedArraysExample2(t *testing.T) {
 	actual := findMedianSortedArrays(nums1, nums2)
 	assert.Equal(t, 2.5, actual)
 }
+func TestFindMedianSortedArrays2MiddleNumsInSameSlice(t *testing.T) {
+	nums1 := []int{1, 3, 5}
+	nums2 := []int{0}
+
+	actual := findMedianSortedArrays(nums1, nums2)
+	assert.Equal(t, 2.0, actual)
+}
