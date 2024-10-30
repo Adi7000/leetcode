7199861b99762cfc0aea4f4d6d307d7f0a19cc21
Aditya Goyal <agoyal7000@gmail.com>
2024-10-29 21:20:26 -0600

    This is a combination of 7 commits.
    TI-150 17.Letter Combinations Added tests

---
+++ src/letter_combinations_test.go
@@ -0,0 +1,42 @@
+package leetcode
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestLetterCombinationsExample1(t *testing.T) {
+	nums := "23"
+	actual := letterCombinations(nums)
+	expected := []string{
+		"ad", "ae", "af",
+		"bd", "be", "bf",
+		"cd", "ce", "cf",
+	}
+	assert.Equal(t, expected, actual)
+}
+func TestLetterCombinationsExample2(t *testing.T) {
+	nums := ""
+	actual := letterCombinations(nums)
+	expected := []string{}
+	assert.Equal(t, expected, actual)
+}
+func TestLetterCombinationsExample3(t *testing.T) {
+	nums := "2"
+	actual := letterCombinations(nums)
+	expected := []string{"a", "b", "c"}
+	assert.Equal(t, expected, actual)
+}
+func TestLetterCombinations4LetterDigit(t *testing.T) {
+	nums := "9"
+	actual := letterCombinations(nums)
+	expected := []string{"w", "x", "y", "z"}
+	assert.Equal(t, expected, actual)
+}
+func TestInvalidCharacter(t *testing.T) {
+	nums := "1"
+	actual := letterCombinations(nums)
+	expected := []string{}
+	assert.Equal(t, expected, actual)
+}
