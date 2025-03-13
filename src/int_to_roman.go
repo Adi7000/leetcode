2d480c8c236b5ec39618f15771395471ce3b36e7
Aditya Goyal <agoyal7000@gmail.com>
2025-03-12 18:45:54 -0600

    TI-150 12.Integer to Roman Refactored
        Still in O(log n). Used paired lists

--- src/int_to_roman.go
+++ src/int_to_roman.go
@@ -1,49 +1,16 @@
 package leetcode
 
 func IntToRoman(num int) string {
+	numThresholds := []int{1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1}
+	romanSymbols := []string{"M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"}
 	romanStr := ""
+	thresholdI := 0
 	for num > 0 {
-		switch {
-		case num >= 1000:
-			romanStr += "M"
-			num -= 1000
-		case num >= 900:
-			romanStr += "CM"
-			num -= 900
-		case num >= 500:
-			romanStr += "D"
-			num -= 500
-		case num >= 400:
-			romanStr += "CD"
-			num -= 400
-		case num >= 100:
-			romanStr += "C"
-			num -= 100
-		case num >= 90:
-			romanStr += "XC"
-			num -= 90
-		case num >= 50:
-			romanStr += "L"
-			num -= 50
-		case num >= 40:
-			romanStr += "XL"
-			num -= 40
-		case num >= 10:
-			romanStr += "X"
-			num -= 10
-		case num >= 9:
-			romanStr += "IX"
-			num -= 9
-		case num >= 5:
-			romanStr += "V"
-			num -= 5
-		case num >= 4:
-			romanStr += "IV"
-			num -= 4
-		case num >= 1:
-			romanStr += "I"
-			num -= 1
-
+		if num >= numThresholds[thresholdI] {
+			romanStr += romanSymbols[thresholdI]
+			num -= numThresholds[thresholdI]
+		} else {
+			thresholdI++
 		}
 
 	}
