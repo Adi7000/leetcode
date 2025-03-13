6ae09cd4e3977f3248fc2d9e7c544fd4ff6762e4
Aditya Goyal <agoyal7000@gmail.com>
2025-03-12 18:48:38 -0600

    TI-150 12.Integer to Roman added more tests

--- src/int_to_roman_test.go
+++ src/int_to_roman_test.go
@@ -6,9 +6,21 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
+func TestIntToRomanExample1(t *testing.T) {
+	num := 3749
+	actual := IntToRoman(num)
+	assert.Equal(t, "MMMDCCXLIX", actual)
+
+}
 func TestIntToRomanExample2(t *testing.T) {
 	num := 58
 	actual := IntToRoman(num)
 	assert.Equal(t, "LVIII", actual)
+
+}
+func TestIntToRomanExample3(t *testing.T) {
+	num := 1994
+	actual := IntToRoman(num)
+	assert.Equal(t, "MCMXCIV", actual)
 
 }
